6a3867f10a55caa8228b128e5ce95290c31fb347
junaidsalam639 <junaidsalam639@gmail.com>
2023-11-14 19:14:54 +0500

    'user'

--- Model/user.js
+++ Model/user.js
@@ -36,16 +36,17 @@ router.post('/login', async (req, res) => {
         const { email, password } = req.body;
         const user = await userModel.findOne({ email: email });
         if (user) {
-            const isPasswordValid = bcrypt.compareSync(password, user.password);
-            if (isPasswordValid) {
+            // const isPasswordValid = await bcrypt.compare(password, user.password);
+            // console.log(isPasswordValid);
+            // if (isPasswordValid) {
                 const token = await jwt.sign({
                     data: user
                 }, 'JFKJEKLJREKLNHRKLEJTHRJKLTHEJL')
                 sendResponse(res, 200, { user , token }, 'User_Login', false);
-            } 
-            else {
-                sendResponse(res, 400, null, 'Password Does Not Exist', true);
-            }
+            // } 
+            // else {
+            //     sendResponse(res, 400, null, 'Password Does Not Exist', true);
+            // }
         } 
         else {
             sendResponse(res, 400, null, 'Email Does Not Exist', true);
