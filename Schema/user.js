aa81cb22fc10c51a3cf003db63160b9c509e8d03
Hammad <hammadurrehman70@gmail.com>
2023-11-16 17:57:29 +0500

    'api'

--- Schema/user.js
+++ Schema/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose
 
 const userSchema = new Schema({
-    username: { type: String },
+    username: { type: String , required : true },
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
 }, {timestamps : true});
